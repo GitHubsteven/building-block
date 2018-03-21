d7cda21e1b1b44358644cc7597fc3dc467850661
rongbin.xie <rongbin.xie@voyageone.cn>
2018-03-21 17:00:55 +0800

    try spring

---
+++ service/src.java.main/bean/OrderModel.java
@@ -0,0 +1,20 @@
+package bean;
+
+
+/**
+ * order model
+ *
+ * Created by rongbin.xie on 2018/3/21.
+ */
+public class OrderModel {
+    private  Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public OrderModel setId(Long id) {
+        this.id = id;
+        return this;
+    }
+}
--- service/src.java.main/service/SortService.java
+++ service/src.java.main/service/SortService.java
@@ -2,12 +2,12 @@ package service;
 
 /**
  * sort service
- *
+ * <p>
  * Created by rongbin.xie on 2018/3/21.
  */
 @org.springframework.stereotype.Service
 public class SortService {
-    int[] sort(int[] numbers){
+    int[] sort(int[] numbers) {
         System.out.println("sort numbers ...");
         return numbers;
     }
