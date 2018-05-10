40ebc802df494c9aed7d0cb4f755f4307527b940
rongbin.xie <rongbin.xie@voyageone.cn>
2018-05-10 14:01:44 +0800

    + memento design pattern case

--- java-design-pattern/src/main/java/tranditional/memento/GamePlayer.java
+++ java-design-pattern/src/main/java/tranditional/memento/GamePlayer.java
@@ -1,46 +1,44 @@
 package tranditional.memento;
 
+import lombok.Data;
+
 /**
  * @version 1.0.0 COPYRIGHT © 2001 - 2018 VOYAGE ONE GROUP INC. ALL RIGHTS RESERVED.
  * @Author rongbin.xie
  * @Description:
  * @Date: Created at 13:06 2018/5/10.
  */
+@Data
 public class GamePlayer {
     private Integer bloodFlow;
     private Integer magicFlow;
     private Integer level;
     private String scene;
 
-    public Integer getBloodFlow() {
-        return bloodFlow;
+    public void revert(Memento memento) {
+        this.bloodFlow = memento.getBloodFlow();
+        this.magicFlow = memento.getMagicFlow();
+        this.level = memento.getLevel();
+        this.scene = memento.getScene();
     }
 
-    public void setBloodFlow(Integer bloodFlow) {
-        this.bloodFlow = bloodFlow;
+    public Memento memento() {
+        Memento memento = new Memento();
+        memento.setScene(scene);
+        memento.setLevel(level);
+        memento.setMagicFlow(magicFlow);
+        memento.setBloodFlow(bloodFlow);
+        return memento;
     }
 
-    public Integer getMagicFlow() {
-        return magicFlow;
+    @Override
+    public String toString() {
+        return "GamePlayer{" +
+                "bloodFlow=" + bloodFlow +
+                ", magicFlow=" + magicFlow +
+                ", level=" + level +
+                ", scene='" + scene + '\'' +
+                '}';
     }
 
-    public void setMagicFlow(Integer magicFlow) {
-        this.magicFlow = magicFlow;
-    }
-
-    public Integer getLevel() {
-        return level;
-    }
-
-    public void setLevel(Integer level) {
-        this.level = level;
-    }
-
-    public String getScene() {
-        return scene;
-    }
-
-    public void setScene(String scene) {
-        this.scene = scene;
-    }
 }
--- java-design-pattern/src/main/java/tranditional/memento/TestEvor.java
+++ java-design-pattern/src/main/java/tranditional/memento/TestEvor.java
@@ -7,4 +7,28 @@ package tranditional.memento;
  * @Date: Created at 13:48 2018/5/10.
  */
 public class TestEvor {
+    public static void main(String[] args) {
+        GamePlayer playerHere = new GamePlayer();
+        playerHere.setBloodFlow(100);
+        playerHere.setLevel(1);
+        playerHere.setMagicFlow(100);
+        playerHere.setScene("here");
+        System.out.println(playerHere);
+        System.out.println("============player fight with a ghost, cost 200 bp and 10 mp, now is in the fight scene============");
+        CareTaker careTaker = new CareTaker();
+        careTaker.addMemento(playerHere.memento());
+
+        playerHere.setBloodFlow(-100);
+        playerHere.setMagicFlow(80);
+        playerHere.setLevel(3);
+        playerHere.setScene("fight scene");
+        System.out.println(playerHere.toString());
+
+        if (playerHere.getBloodFlow() < 0) {
+            System.out.println("player is dead! Player will be relived");
+            playerHere.revert(careTaker.getMementoById(1));
+        }
+        System.out.println("player revert!");
+        System.out.println(playerHere.toString());
+    }
 }
