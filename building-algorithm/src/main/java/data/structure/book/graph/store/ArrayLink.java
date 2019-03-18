54bd8cd1eed106115b47f582ae4d99588589a51a
rongbin.xie <xrb16302@163.com>
2019-03-19 01:11:31 +0800

    + 数组/链表来表示图

--- building-algorithm/src/main/java/data/structure/book/graph/store/ArrayLink.java
+++ building-algorithm/src/main/java/data/structure/book/graph/store/ArrayLink.java
@@ -3,6 +3,8 @@ package data.structure.book.graph.store;
 import bean.GraphicArc;
 import bean.GraphicVertex;
 import data.structure.book.graph.conduct.IGraphicOperation;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.List;
 
@@ -13,7 +15,15 @@ import java.util.List;
  * @version: 1.0.0
  * @description: 数组+ 链表表示图
  **/
+@Setter
+@Getter
 public class ArrayLink<T extends Comparable<T>> implements IGraphicOperation<T> {
+    /**
+     * 定点数组
+     */
+    private List<GraphicVertex> vertices;
+
+
     @Override
     public void createGraph(List<GraphicVertex<T>> graphicVertices, List<GraphicArc<T>> graphicArcs,
                             boolean isDirected, boolean isWithWeight) {
