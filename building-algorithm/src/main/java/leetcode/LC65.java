667222ad1bad979b8442a5409f92c62894607830
rongbin.xie <xrb16302@163.com>
2019-04-13 12:36:50 +0800

    + leetcode 65

--- building-algorithm/src/main/java/leetcode/LC65.java
+++ building-algorithm/src/main/java/leetcode/LC65.java
@@ -1,5 +1,7 @@
 package leetcode;
 
+import java.util.Objects;
+
 /**
  * @version 1.0.0 COPYRIGHT © 2001 - 2018 VOYAGE ONE GROUP INC. ALL RIGHTS RESERVED.
  * @Author jet.xie
@@ -8,7 +10,34 @@ package leetcode;
  */
 public class LC65 {
     public static void main(String[] args) {
-
+        String example = "\"0\" => true\n" +
+                "\" 0.1 \" => true\n" +
+                "\"abc\" => false\n" +
+                "\"1 a\" => false\n" +
+                "\"2e10\" => true\n" +
+                "\" -90e3   \" => true\n" +
+                "\" 1e\" => false\n" +
+                "\"e3\" => false\n" +
+                "\" 6e-1\" => true\n" +
+                "\" 99e2.5 \" => false\n" +
+                "\"53.5e93\" => true\n" +
+                "\" --6 \" => false\n" +
+                "\"-+3\" => false\n" +
+                "\".1\" => true\n" +
+                "\"95a54e53\" => false";
+        String[] examples = example.replaceAll("\"", "").split("\n");
+        for (String exea : examples) {
+            String[] values = exea.split(" => ");
+            if (values.length != 2) {
+                System.out.println("err split:" + exea);
+                continue;
+            }
+            boolean expectedResult = Objects.equals(values[1].trim(), "true");
+            if (expectedResult != isNumber(values[0])) {
+                System.out.println("err test case:" + exea);
+            }
+        }
+        System.out.println(isNumber("+.8"));
     }
 
     /**
@@ -31,29 +60,81 @@ public class LC65 {
      * @return
      */
     public static boolean isNumber(String s) {
-        char POINT = '.';
-        char
         if (s == null || s.length() == 0) return false;
-        char first = s.charAt(0);
-        if ((first < '0' || first > '9') && first !='') return false;
-        boolean isBase = true;
-        int length = s.length();
-        for (int i = 1; i < length; i++) {
-            char letter = s.charAt(i);
-            if (letter == 'e') {
-                if (i == length - 1) return false;
-                isBase = false;
-                continue;
-            }
-            if (isBase) {
-                if (letter == POINT) {
-                    char next = s.charAt(i + 1);
-                    if (next < '0' || next > '9') return false;
-                }
+        String realStr = s.trim();
+        if (realStr.length() == 0) return false;
+        char POINT = '.';
+        char MINUS_LETTER = '-';
+        char ADD_LETTER = '+';
+        char SPLIT_LETTER = 'e';
+        //对于一个字符串是否为合法的数值标志
+        boolean isSigned = false;
+        boolean isPoint = false;
+        boolean hasVal;
+        //对于第一个值开始判断
+        char first = realStr.charAt(0);
+        if ((first > '9' || first < '0')) {
+            if (first == MINUS_LETTER || first == ADD_LETTER) {
+                isSigned = true;
+            } else if (first == POINT) {
+                isPoint = true;
             } else {
-
+                return false;
             }
         }
-        return true;
+        hasVal = !isSigned && !isPoint;
+        //对于一个字符是否为底数还是指数
+        boolean isExponent = false;
+        boolean isExpSigned = false;
+        boolean expHasValue = false;
+        boolean isExp1st = true;
+        int length = realStr.length();
+        for (int i = 1; i < length; i++) {
+            char letter = realStr.charAt(i);
+            if (letter == SPLIT_LETTER) {
+                if (!hasVal || isExponent) return false;
+                if (i == length - 1) return false;
+                isExponent = true;
+                continue;
+            }
+            if (isExponent) {
+                if ('0' <= letter && letter <= '9') {
+                    expHasValue = true;
+                    isExp1st = false;
+                } else {
+                    if (letter == '-' || letter == '+') {
+                        if (isExpSigned) return false;
+                        if (isExp1st) {
+                            isExpSigned = true;
+                            isExp1st = false;
+                        } else {
+                            return false;
+                        }
+                    } else {
+                        return false;
+                    }
+                }
+            } else {
+                if ('0' <= letter && letter <= '9') {
+                    hasVal = true;
+                } else {
+                    if (letter == POINT) {
+                        if (isSigned && i == 1) continue;
+                        if (hasVal) {
+                            if (!isPoint) {
+                                isPoint = true;
+                                continue;
+                            } else return false;
+                        }
+                        return false;
+                    } else if (letter == MINUS_LETTER || letter == ADD_LETTER) {
+                        return false;
+                    } else {
+                        return false;
+                    }
+                }
+            }
+        }
+        return hasVal && (!isExponent || expHasValue);
     }
 }
