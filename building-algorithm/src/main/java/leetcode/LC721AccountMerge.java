dadad007b67c67026e374915446ed37601639b13
GitHubsteven <xrb16302@163.com>
2020-03-03 09:40:29 +0800

    721 待测试

--- building-algorithm/src/main/java/leetcode/LC721AccountMerge.java
+++ building-algorithm/src/main/java/leetcode/LC721AccountMerge.java
@@ -1,5 +1,6 @@
 package leetcode;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -12,15 +13,54 @@ public class LC721AccountMerge {
 
     }
 
-    void dfs(int index, boolean[][] matrix) {
-        boolean[] isVisited = new boolean[]
-    }
-
-
     public List<List<String>> accountsMerge(List<List<String>> accounts) {
         boolean[][] adjacentMatrix = buildAdjacentMatrix(accounts);
         boolean[] isVisited = new boolean[accounts.size()];
-        return null;
+        //loop and recurse
+        List<List<String>> mergedAccounts = new ArrayList<>();
+        for (int index = 0; index < accounts.size(); index++) {
+            if (isVisited[index]) continue;
+            List<Integer> sameAccountIndexes = new ArrayList<>();
+            dfs(adjacentMatrix, isVisited, index, sameAccountIndexes);
+            List<String> newAccount = mergeAllSameAccounts(accounts, sameAccountIndexes);
+            mergedAccounts.add(newAccount);
+        }
+        return mergedAccounts;
+    }
+
+    private List<String> mergeAllSameAccounts(List<List<String>> allAccount, List<Integer> sameAccountIndex) {
+        List<String> first = allAccount.get(0);
+        for (int i = 1; i < sameAccountIndex.size(); i++) {
+            List<String> second = allAccount.get(i);
+            mergeAccount(first, second);
+        }
+        return first;
+    }
+
+    /**
+     * merge two account
+     *
+     * @param first  account merged to
+     * @param merged account to be merged
+     */
+    public void mergeAccount(List<String> first, List<String> merged) {
+        String firstName = first.get(0);
+        String mergedName = merged.get(0);
+        if (firstName.compareTo(mergedName) > 0) first.set(0, mergedName);
+        for (int index = 1; index < merged.size(); index++) {
+            if (!first.contains(merged.get(index))) first.add(merged.get(index));
+        }
+    }
+
+    public void dfs(boolean[][] adjacentMatrix, boolean[] isVisited, int start, List<Integer> visitor) {
+        if (isVisited[start]) return;
+        visitor.add(start);
+        isVisited[start] = true;
+        boolean[] directSameAccIndexes = adjacentMatrix[start];
+        for (int idx = 0; idx < directSameAccIndexes.length; idx++) {
+            if (directSameAccIndexes[idx] && !isVisited[idx])
+                dfs(adjacentMatrix, isVisited, start, visitor);
+        }
     }
 
     private boolean[][] buildAdjacentMatrix(List<List<String>> accounts) {
