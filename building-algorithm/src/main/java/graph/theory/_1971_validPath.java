e7085c2607ab693537b37e4e387d3c8deadcb9cc
rongbinxie <rongbin.xie@voyageone.cn>
2022-02-23 18:45:46 +0800

    1971_oom_solution

---
+++ building-algorithm/src/main/java/graph/theory/_1971_validPath.java
@@ -0,0 +1,92 @@
+package graph.theory;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * @author rongbin.xie
+ * @version 1.0.0
+ * @date 2022/2/23
+ * @description
+ * @copyright COPYRIGHT © 2014 - 2021/5/18 VOYAGE ONE GROUP INC. ALL RIGHTS RESERVED.
+ **/
+public class _1971_validPath {
+    public static void main(String[] args) {
+        int n = 30000, start = 18473, end = 26690;
+        int[][] edges = {{0, 1}, {0, 2}, {1, 2}};
+
+        System.out.println(validPath(n, edges, start, end));
+    }
+
+    public static boolean validPath(int n, int[][] edges, int source, int destination) {
+        boolean[][] isConnected = new boolean[n][n];
+        // 初始化边
+        for (int[] edge : edges) {
+            isConnected[edge[0]][edge[1]] = true;
+            isConnected[edge[1]][edge[0]] = true;
+        }
+        /*
+        查看当前节点的下一节点是否包含目标节点，如果是那么表示可以到达，否则不可以
+         */
+        boolean[] hasVisited = new boolean[n];
+        hasVisited[destination] = true;
+        return isCurConnected(isConnected, destination, source, hasVisited);
+    }
+
+    static boolean isCurConnected(boolean[][] isConnected, int cur, int source, boolean[] hasVisited) {
+        System.out.println(cur);
+        if (cur < 0) return false;
+        if (cur >= isConnected.length) return false;
+        if (isConnected[cur][source]) {
+            hasVisited[cur] = true;
+            return true;
+        }
+        boolean isArrive = false;
+        for (int i = 0; i < isConnected.length; i++) {
+            if (cur == i) continue;
+            if (isConnected[cur][i]) {
+                if (hasVisited[i]) continue;
+                hasVisited[i] = true;
+                isArrive = isCurConnected(isConnected, i, source, hasVisited);
+                if (isArrive) break;
+                hasVisited[i] = true;
+            }
+        }
+        hasVisited[cur] = true;
+        return isArrive;
+    }
+
+
+    public boolean online(int n, int[][] edges, int start, int end) {
+        boolean cameBefore[] = new boolean[n];
+        Map<Integer, Set<Integer>> map = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            map.put(i, new HashSet<>());
+        }
+        for (int i = 0; i < edges.length; i++) {
+            map.get(edges[i][0]).add(edges[i][1]);
+            map.get(edges[i][1]).add(edges[i][0]);
+        }
+        return hasPath(edges, start, end, cameBefore, map);
+    }
+
+    public boolean hasPath(int[][] edges, int start, int end, boolean[] cameBefore, Map<Integer, Set<Integer>> map) {
+        if (start == end) {
+            return true;
+        }
+        if (cameBefore[start]) {
+            return false;
+        }
+        cameBefore[start] = true;
+        for (int a : map.get(start)) {
+            if (hasPath(edges, a, end, cameBefore, map)) {
+                return true;
+            }
+        }
+        //cameBefore[start]=true  注意此处没有必要，如果之前的线路来过发现不通，那么后来的线路也不通，如果加上反而会超时
+        return false;
+    }
+
+}
--- building-algorithm/src/main/java/leetcode/greedy/_253_min_meeting_rooms.java
+++ building-algorithm/src/main/java/leetcode/greedy/_253_min_meeting_rooms.java
@@ -1,34 +1,67 @@
 package leetcode.greedy;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class _253_min_meeting_rooms {
     public static void main(String[] args) {
-        System.out.println(Math.abs(-1));
+        int[][] meetings = {{7, 10}, {2, 4}};
+        System.out.println(bruce_force(meetings));
     }
 
     public static int minMeetingRooms(int[][] intervals) {
-        // TODO: 2022/2/16
-        int count = 1;
-        List<int[]> list = new ArrayList<>();
-        list.add(intervals[0]);
-        int index = -1, min;
-        for (int j = 1; j < intervals.length; j++) {
-            min = Integer.MAX_VALUE;
-            index = -1;
-            for (int i = 0; i < count; i++) {
-                // 如果当前开始时间>会议室被占用的起始时间，那么就插到前面，然后记录当前间隔时间为最小浪费时间
-                if (intervals[j][1] < list.get(i)[0]) {
-                    if (Math.abs(list.get(i)[0] - intervals[j][1]) < min) {
-                        index = i;
-                        min = list.get(i)[0] = intervals[j][0];
-                    }
-                } else if (intervals[j][0] > list.get(i)[1]) {
+        return 0;
+    }
 
+    /**
+     * 暴力枚举：并不能满足需求
+     *
+     * @param intervals 会议时间
+     */
+    public static int bruce_force(int[][] intervals) {
+        int count = 1;
+        List<List<int[]>> roomsInNeed = new ArrayList<>();
+        roomsInNeed.add(createNewRoom(intervals[0]));
+        for (int i = 1; i < intervals.length; i++) {
+            int start = intervals[i][0];
+            int end = intervals[i][1];
+            boolean isArranged = false;
+            // 遍历会议室
+            for (int j = 0; j < count; j++) {
+                List<int[]> room = roomsInNeed.get(j);
+                // 遍历当前会议室所有会议，看是否可以安排进去
+                for (int x = 0; x < room.size() - 1; x++) {
+                    // 判断是否可以在当前会议和后面一个会议之间，如果可以，那么插入这个会议之后
+                    if (room.get(x)[1] < start && room.get(x + 1)[0] > end) {
+                        room.add(x + 1, intervals[i]);
+                        isArranged = true;
+                        break;
+                    }
                 }
+                // 判断是否安排了会议室，如果安排了，中断后续会议室的遍历
+                if (isArranged) break;
+            }
+            // 如果未安排，那么需要新的会议室
+            if (!isArranged) {
+                roomsInNeed.add(createNewRoom(intervals[i]));
+                count++;
             }
         }
-        return 0;
+        return count;
+    }
+
+    /**
+     * 创建一个新的会议室，但是会议室必须有至少一个会议室，并且开头和结尾都增加了两个空会议，作为哨兵
+     *
+     * @param firstMeeting 第一个会议
+     * @return 会议室
+     */
+    static List<int[]> createNewRoom(int[] firstMeeting) {
+        List<int[]> room = new LinkedList<>();
+        room.add(new int[]{0, 0});
+        room.add(firstMeeting);
+        room.add(new int[]{Integer.MAX_VALUE, Integer.MAX_VALUE});
+        return room;
     }
 }
